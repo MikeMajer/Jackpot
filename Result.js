654d7e6e5671e89187fd0bfa946a7101abaaac67
Mike Majer <majer.michal@outlook.com>
2019-07-31 17:55:09 +1000

    Layout correction

--- Result.js
+++ Result.js
@@ -1,12 +1,12 @@
 class Result {
- static moneyWinInGame(result, bid) {
-  if (result) return 3 * bid;
-  else return 0;
- }
+  static moneyWinInGame(result, bid) {
+    if (result) return 2 * bid;
+    else return 0;
+  }
 
- static checkWinner(draw) {
-  if (draw[0] === draw[1] && draw[1] === draw[2] || draw[0] !== draw[1] && draw[1] !== draw[2] && draw[0] !== draw[2]) return true;
-  else return false
- }
+  static checkWinner(draw) {
+    if (draw[0] === draw[1] && draw[1] === draw[2] || draw[0] !== draw[1] && draw[1] !== draw[2] && draw[0] !== draw[2]) return true;
+    else return false
+  }
 }
 
